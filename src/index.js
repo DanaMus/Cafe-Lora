5a0a51ca8e1525bd1a2824249d07b639a751a037
Dana Musilova <dana.musilova@email.cz>
2021-05-01 12:08:28 +0200

    Oprava chyby

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navElm = document.querySelector('.nav-btn');
+const navBarElm = document.querySelector('nav');
+const navLinkElm = document.querySelectorAll('nav a');
+
+navElm.addEventListener('click', () => {
+  navBarElm.classList.toggle('nav-closed');
+});
+
+for (let i = 0; i < navLinkElm.length; i++) {
+  navLinkElm[i].addEventListener('click', () => {
+    navBarElm.classList.add('nav-closed');
+  });
+}
